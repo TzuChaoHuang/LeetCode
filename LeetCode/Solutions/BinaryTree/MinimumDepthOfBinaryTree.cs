84a8dfc396ddc3a3b041aee1fc89a7df46ebd021
Leo.Huang <leo12160201@gmail.com>
2025-07-06 20:18:48 +0800

    111. Minimum Depth of Binary Tree

--- LeetCode/Solutions/BinaryTree/MinimumDepthOfBinaryTree.cs
+++ LeetCode/Solutions/BinaryTree/MinimumDepthOfBinaryTree.cs
@@ -2,6 +2,12 @@ namespace LeetCode.Solutions;
 
 public class MinimumDepthOfBinaryTree
 {
+    /// <summary>
+    /// Given a binary tree, find its minimum depth.
+    /// https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
     public int MinDepth(TreeNode root)
     {
         int path = 0;
